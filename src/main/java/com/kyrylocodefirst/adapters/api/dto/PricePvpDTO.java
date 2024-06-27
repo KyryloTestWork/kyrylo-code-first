97b3d4d3e62693b2cc6db020a5a48b55a8f895dc
KyryloTestWork <trayderupone@gmail.com>
2024-06-28 00:44:39 +0200

    Update PricePvpDTO.java
    Add imports

--- src/main/java/com/kyrylocodefirst/adapters/api/dto/PricePvpDTO.java
+++ src/main/java/com/kyrylocodefirst/adapters/api/dto/PricePvpDTO.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
